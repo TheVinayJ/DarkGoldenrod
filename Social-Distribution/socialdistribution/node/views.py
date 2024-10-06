3421b3a9d457811d349851d7e54f8e4127f6bcd6
TheVinayJ <thevinayj@gmail.com>
2024-10-06 14:11:24 -0600

    More boilerplate stuff from Lab 2. MARKDOWN EDITOR NOT YET INCLUDED

--- Social-Distribution/socialdistribution/node/views.py
+++ Social-Distribution/socialdistribution/node/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse
+from.models import Post
 
-# Create your views here.
+# Index taken from Lab 2
+# Needs to be changed, just using for testing purposes
+def index(request):
+    posts = []
+
+    post_objects = Post.objects.all()
+    for post in post_objects:
+        posts.append({
+            "title": post.title,
+            "url": reverse("view", kwargs={ "id": post.id })
+        })
+    return render(request, "index.html", { "posts": posts })
+
+def editor(request):
+    return render(request, "editor.html")
