bc767ddea0619ed8e3ebd83d93fc635e14fa7229
TheVinayJ <thevinayj@gmail.com>
2024-10-10 14:16:27 -0600

    Abstract likes instead of generic ones

--- Social-Distribution/socialdistribution/node/models.py
+++ Social-Distribution/socialdistribution/node/models.py
@@ -1,8 +1,6 @@
 from email._header_value_parser import ContentType
 
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 
 
 # Create your models here.
@@ -16,10 +14,16 @@ class Author(models.Model):
     friends = models.ManyToManyField('Author')
 
 class Like(models.Model):
-    # Generic Foreign key taken from django documentation
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
+
+    class Meta:
+        abstract = True
+
+class PostLike(Like):
+    owner = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class CommentLike(Like):
+    owner = models.ForeignKey(Comment, on_delete=models.CASCADE)
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
@@ -28,7 +32,6 @@ class Post(models.Model):
     description = models.TextField() # Posts need a short description
     text_content = models.TextField() # Text post content (optional)
     image_content = models.TextField() # Link to image
-    likes = GenericRelation(Like)
     published = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
@@ -36,4 +39,3 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     text = models.TextField()
-    likes = GenericRelation(Like)
