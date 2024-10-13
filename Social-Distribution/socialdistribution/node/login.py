b250d5f40e35c385c4b92fcda52dd6ae404aea92
BNKDuy <100258881+BNKDuy@users.noreply.github.com>
2024-10-13 12:43:51 -0600

    Update login.py to set session id (cookies))

--- Social-Distribution/socialdistribution/node/login.py
+++ Social-Distribution/socialdistribution/node/login.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 from .models import Author
+from django.contrib.auth.hashers import make_password, check_password
 
 def email_input_view(request):
     if request.method == 'POST':
@@ -19,13 +20,13 @@ def login_with_display_name_view(request):
     display_name = request.GET.get('display_name', '')
     error = None
     if request.method == 'POST':
-        display_name = request.POST.get('display_name')
         password = request.POST.get('password')
         try:
             author = Author.objects.get(display_name=display_name)
-            if author.password == password:
-                # Password matches, login successful
-                # You can set session data here if needed
+            if check_password(password, author.password):
+                # Password is correct
+                request.session['author_id'] = author.id
+                request.session['display_name'] = author.display_name
                 return HttpResponse(f"Welcome back, {author.display_name}!")
             else:
                 error = 'Invalid password'
@@ -33,12 +34,14 @@ def login_with_display_name_view(request):
             error = 'User does not exist'
     return render(request, 'login_page.html', {'display_name': display_name, 'error': error})
 
+
 def signup_view(request):
     if request.method == 'POST':
         display_name = request.POST.get('display_name')
         email = request.POST.get('email')
         password = request.POST.get('password')
         description = request.POST.get('description', '')
+        
         # Check if display_name or email already exists
         if Author.objects.filter(display_name=display_name).exists():
             error = 'Display name already taken'
@@ -46,14 +49,20 @@ def signup_view(request):
         if Author.objects.filter(email=email).exists():
             error = 'Email already registered'
             return render(request, 'signup_page.html', {'error': error})
+        
+        # Hash the password
+        hashed_password = make_password(password)
+        
         # Create new Author
         author = Author.objects.create(
             display_name=display_name,
             email=email,
-            password=password,
+            password=hashed_password,
             description=description
-            # Other fields can be set to default or left blank
         )
+        
         # Login the user (you may set session data here)
+        request.session['author_id'] = author.id
+        request.session['display_name'] = author.display_name
         return HttpResponse(f"Account created for {author.display_name}!")
     return render(request, 'signup_page.html')
