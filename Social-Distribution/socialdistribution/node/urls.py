2f99d862ad9fe5b2ff0d7ff4e0574224a61ff7b8
BNKDuy <100258881+BNKDuy@users.noreply.github.com>
2024-10-13 22:30:30 -0600

    Update urls.py

--- Social-Distribution/socialdistribution/node/urls.py
+++ Social-Distribution/socialdistribution/node/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("posts/<int:post_id>/", views.view_post, name="view_post"),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('<int:user_id>/profile/', views.profile, name='profile'),
-    path('enter-email/', login.email_input_view, name='enter_email'),
-    path('login/', login.login_with_display_name_view, name='login_with_display_name'),
-    path('signup/', login.signup_view, name='signup'),
+    path('login/', login.login, name='login'),
+    path('signup/', login.signup, name='signup'),
 ]
