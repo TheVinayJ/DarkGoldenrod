dc27ea11c31be65f8e63ff297e1bc4cd2fc2c33a
BNKDuy <100258881+BNKDuy@users.noreply.github.com>
2024-11-03 12:21:24 -0700

    js file to get token

---
+++ webapp/CSRFToken.js
@@ -0,0 +1,6 @@
+function getCSRFToken() {
+    const cookie = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('csrftoken='));
+    return cookie ? cookie.split('=')[1] : '';
+}
