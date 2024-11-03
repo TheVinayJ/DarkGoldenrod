cbcc86b6b66d05c5c82f563d1845320edb6c2584
BNKDuy <100258881+BNKDuy@users.noreply.github.com>
2024-11-03 12:49:21 -0700

    Update CSRFToken.js

--- webapp/CSRFToken.js
+++ webapp/CSRFToken.js
@@ -4,3 +4,10 @@ function getCSRFToken() {
         .find(row => row.startsWith('csrftoken='));
     return cookie ? cookie.split('=')[1] : '';
 }
+
+function getAuthorId() {
+    const cookie = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('author_id='));
+    return cookie ? cookie.split('=')[1] : null;
+}
