b6339859d349d03a35751f08138fccc6454027f5
BNKDuy <100258881+BNKDuy@users.noreply.github.com>
2024-11-04 12:50:36 -0700

    Update urls.py

--- Social-Distribution/socialdistribution/node/urls.py
+++ Social-Distribution/socialdistribution/node/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
     path('api/user-info/', UserInfoView.as_view(), name='user_info'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('posts/<int:post_id>', views.get_post, name='get_post'),
-    path('authors/<int:author_id>/posts', views.author_posts, name='author_posts'),
+    path('api/authors/<int:author_id>/posts', views.author_posts, name='author_posts'),
     path('authors/<int:author_id>/posts/<int:post_id>', views.api_get_post_from_author, name='api_get_post_from_author'),
     path('api/authors/<int:author_id>/posts/<inv:post_id>/likes', views.get_post_likes, name='get_post_likes'),
 ]
