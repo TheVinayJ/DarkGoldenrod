3ab5adfdbec2ffd15d88b3502f6070d9bee4810a
TheVinayJ <thevinayj@gmail.com>
2024-11-13 01:05:42 -0700

    Restored commented-out tests

--- Social-Distribution/socialdistribution/node/tests/test_posts.py
+++ Social-Distribution/socialdistribution/node/tests/test_posts.py
@@ -80,80 +80,80 @@ class PostTests(TestCase):
         Author.objects.all().delete()
         self.client.cookies.clear()
 
-    # def test_post_creation(self):
-    #         self.assertEqual(self.post.title, "Test Title")
-    #         self.assertEqual(self.post.author.display_name, "Test Author")
-    #
-    # def test_add_post(self):
-    #         response = self.client.get(reverse('add'), follow=True)
-    #         self.assertEqual(response.status_code, 200)
-    #         response = self.client.post(f'api/authors/{self.author.id}/posts', {'title': 'New Post',
-    #                                                         'body-text': 'Test Description',
-    #                                                         'visibility': 'PUBLIC',
-    #                                                         'contentType': 'text/plain',
-    #                                                         'content': 'Test Content',
-    #                                                         'description': 'Test Description',
-    #                                                         'author': self.author,
-    #                                                           })
-    #         self.assertEqual(response.status_code, 302)
-    #         self.assertTrue(Post.objects.filter(title="New Post").exists())
-    #
-    # def test_like_post(self):
+    def test_post_creation(self):
+            self.assertEqual(self.post.title, "Test Title")
+            self.assertEqual(self.post.author.display_name, "Test Author")
+
+    def test_add_post(self):
+            response = self.client.get(reverse('add'), follow=True)
+            self.assertEqual(response.status_code, 200)
+            response = self.client.post(f'api/authors/{self.author.id}/posts', {'title': 'New Post',
+                                                            'body-text': 'Test Description',
+                                                            'visibility': 'PUBLIC',
+                                                            'contentType': 'text/plain',
+                                                            'content': 'Test Content',
+                                                            'description': 'Test Description',
+                                                            'author': self.author,
+                                                              })
+            self.assertEqual(response.status_code, 302)
+            self.assertTrue(Post.objects.filter(title="New Post").exists())
+
+    def test_like_post(self):
+        test_post_id = self.post.id
+        self.assertFalse(PostLike.objects.filter(owner=self.post).exists())
+        response = self.client.post(reverse('like', args=[test_post_id]), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(PostLike.objects.filter(owner=self.post).exists())
+
+    def test_unlike_post(self):
+        test_post_id = self.post.id
+        self.assertFalse(PostLike.objects.filter(owner=self.post).exists())
+        response = self.client.post(reverse('like', args=[test_post_id]), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(PostLike.objects.filter(owner=self.post).exists())
+        response = self.client.post(reverse('like', args=[test_post_id]), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(PostLike.objects.filter(owner=self.post).exists())
+
+    # def test_add_comment(self):
     #     test_post_id = self.post.id
-    #     self.assertFalse(PostLike.objects.filter(owner=self.post).exists())
-    #     response = self.client.post(reverse('like', args=[test_post_id]), follow=True)
+    #     self.assertFalse(Comment.objects.filter(post = self.post).count() > 1)
+    #     response = self.client.post(reverse('add_comment', args=[test_post_id]),
+    #                                 {'content': 'test comment'},
+    #                                 follow=True)
     #     self.assertEqual(response.status_code, 200)
-    #     self.assertTrue(PostLike.objects.filter(owner=self.post).exists())
-    #
-    # def test_unlike_post(self):
-    #     test_post_id = self.post.id
-    #     self.assertFalse(PostLike.objects.filter(owner=self.post).exists())
-    #     response = self.client.post(reverse('like', args=[test_post_id]), follow=True)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTrue(PostLike.objects.filter(owner=self.post).exists())
-    #     response = self.client.post(reverse('like', args=[test_post_id]), follow=True)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertFalse(PostLike.objects.filter(owner=self.post).exists())
-    #
-    # # def test_add_comment(self):
-    # #     test_post_id = self.post.id
-    # #     self.assertFalse(Comment.objects.filter(post = self.post).count() > 1)
-    # #     response = self.client.post(reverse('add_comment', args=[test_post_id]),
-    # #                                 {'content': 'test comment'},
-    # #                                 follow=True)
-    # #     self.assertEqual(response.status_code, 200)
-    # #     self.assertTrue(Comment.objects.filter(post = self.post).count() > 1)
-    #
-    # def test_like_comment(self):
-    #     self.assertFalse(CommentLike.objects.filter(owner=self.comment).exists())
-    #     response = self.client.post(reverse('comment_like'))
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertTrue(CommentLike.objects.filter(owner=self.comment).exists())
-    #
-    # def test_unlike_comment(self):
-    #     self.assertFalse(CommentLike.objects.filter(owner=self.comment).exists())
-    #     response = self.client.post(reverse('comment_like'))
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertTrue(CommentLike.objects.filter(owner=self.comment).exists())
-    #     response = self.client.post(reverse('comment_like'))
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertFalse(CommentLike.objects.filter(owner=self.comment).exists())
-    #
-    # def test_private_post(self):
-    #     response = self.client.get(reverse('view_post', args=[self.private_post.id]), follow=True)
-    #     self.assertEqual(response.status_code, 403)
-    #
-    # def test_friends_post(self):
-    #     response = self.client.get(reverse('view_post', args=[self.friends_post.id]), follow=True)
-    #     self.assertEqual(response.status_code, 403)
-    #     self.following_friend = Follow.objects.create(
-    #         follower=self.author,
-    #         following=self.friend_author)
-    #     self.following_author = Follow.objects.create(
-    #         follower=self.friend_author,
-    #         following=self.author)
-    #     response = self.client.get(reverse('view_post', args=[self.friends_post.id]), follow=True)
-    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTrue(Comment.objects.filter(post = self.post).count() > 1)
+
+    def test_like_comment(self):
+        self.assertFalse(CommentLike.objects.filter(owner=self.comment).exists())
+        response = self.client.post(reverse('comment_like'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(CommentLike.objects.filter(owner=self.comment).exists())
+
+    def test_unlike_comment(self):
+        self.assertFalse(CommentLike.objects.filter(owner=self.comment).exists())
+        response = self.client.post(reverse('comment_like'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(CommentLike.objects.filter(owner=self.comment).exists())
+        response = self.client.post(reverse('comment_like'))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(CommentLike.objects.filter(owner=self.comment).exists())
+
+    def test_private_post(self):
+        response = self.client.get(reverse('view_post', args=[self.private_post.id]), follow=True)
+        self.assertEqual(response.status_code, 403)
+
+    def test_friends_post(self):
+        response = self.client.get(reverse('view_post', args=[self.friends_post.id]), follow=True)
+        self.assertEqual(response.status_code, 403)
+        self.following_friend = Follow.objects.create(
+            follower=self.author,
+            following=self.friend_author)
+        self.following_author = Follow.objects.create(
+            follower=self.friend_author,
+            following=self.author)
+        response = self.client.get(reverse('view_post', args=[self.friends_post.id]), follow=True)
+        self.assertEqual(response.status_code, 200)
 
     def test_get_post_object(self):
         response = views.get_serialized_post(self.post)
