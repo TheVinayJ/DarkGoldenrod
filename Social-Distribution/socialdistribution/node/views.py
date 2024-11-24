bda4811df4b114a98b810f0a7a058d56e244e54c
BNKDuy <100258881+BNKDuy@users.noreply.github.com>
2024-11-24 15:13:39 -0700

    Update views.py

--- Social-Distribution/socialdistribution/node/views.py
+++ Social-Distribution/socialdistribution/node/views.py
@@ -989,8 +989,8 @@ def add_comment(request, id):
 
     new_comment = Comment(post=post, text=text, author=author)
     new_comment.save()
-    followers = Follow.objects.filter(following=f"https://{post.author.host}authors/{post.author.id}")
-    print("Sending comment to people following: ", f"https://{post.author.host}authors/{post.author.id}")
+    followers = Follow.objects.filter(following=f"{post.author.host}authors/{post.author.id}")
+    print("Sending comment to people following: ", f"{post.author.host}authors/{post.author.id}")
     print("Sending comment to: ", followers)
     for follower in followers:
         try:
