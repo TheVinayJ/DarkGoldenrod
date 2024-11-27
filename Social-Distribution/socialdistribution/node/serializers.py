c760698a7d3987dc9fc948e79563f708296953a5
BNKDuy <100258881+BNKDuy@users.noreply.github.com>
2024-11-27 14:51:45 -0700

    Update serializers.py

--- Social-Distribution/socialdistribution/node/serializers.py
+++ Social-Distribution/socialdistribution/node/serializers.py
@@ -304,7 +304,7 @@ class PostSerializer(serializers.ModelSerializer):
         elif obj.contentType.startswith('image') and obj.image_content:
             try:
                 with obj.image_content.open('rb') as image_file:
-                    return f"data:{obj.contentType};base64,{base64.b64encode(image_file.read()).decode('utf-8')}"
+                    return base64.b64encode(image_file.read()).decode('utf-8')
             except FileNotFoundError:
                 return "No image content found."
         return "Nothing to display D:"
